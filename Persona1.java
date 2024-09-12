384867d58dcad3618c192f62abae9aeedd91dd9c
V-1-P-3-R <jariasr1@unicartagena.edu.co>
2024-09-12 16:15:51 -0500

    Update Persona1.java
    Se elimina y se coloca todo con las correccione.

--- Persona1.java
+++ Persona1.java
@@ -1,26 +1,30 @@
-public class Persona1 {
+public class Persona {
     String nombre;
     String piel;
     int edad;
     double altura;
 
     // Constructor para inicializar una persona con altura, edad, nombre y piel
-    public Persona1(String nombre, int edad, double altura, String piel) {
+    public Persona(String nombre, int edad, double altura, String piel) {
         this.altura = altura;
         this.edad = edad;
         this.nombre = nombre;
         this.piel = piel;
     }
+
     // Imprime los datos de la persona
-    public void mostrarDetalles() {
+    public void impresionDeDetalles() {
         System.out.println("Nombre: " + nombre);
         System.out.println("Edad: " + edad + " años");
         System.out.println("Altura: " + altura + " metros");
         System.out.println("Piel: " + piel);
 
+
     }
+
     //Para verificar si es mayor de edad o no
-    public boolean esMayorEdad() {
+    public boolean muestraDeEdad() {
+
         return edad >= 18;
     }
 
@@ -35,37 +39,19 @@ public class Persona1 {
     }
 
 
-
     // Ejecucion principal 1.0
     public static void main(String[] args) {
         // Se crea el primer objeto "Persona1"
-        Persona1 persona1 = new Persona1("Jose", 22, 1.74, "black");
-        persona1.mostrarDetalles();  // Datos de la persona
-        System.out.println("¿Es mayor de edad?: " + (persona1.esMayorEdad() ? "Sí" : "No"));
+        Persona persona1 = new Persona("Jose", 22, 1.74, "black");
+        persona1.impresionDeDetalles();  // Datos de la persona
+        System.out.println("¿Es mayor de edad?: " + (persona1.muestraDeEdad() ? "Sí" : "No"));
+        persona1.anosParaJubilacion();
+
+        // Ejecucion principal 2.0
+        Persona Persona2 = new Persona("Ana", 87, 1.64, "black");
+        Persona2.impresionDeDetalles();
+        System.out.println("¿Es mayor de edad?: " + (persona1.muestraDeEdad() ? "Sí" : "No"));
+        Persona2.anosParaJubilacion();
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 }
